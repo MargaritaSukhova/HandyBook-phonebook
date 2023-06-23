ea75bfc4cf8af9c67fec1d388beb2ae92b7e5592
MargaritaSukhova <margarytasu@gmail.com>
2023-06-24 00:32:24 +0300

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 // import { fetchContacts } from 'redux/operations';
 // import { getError, getIsLoading } from 'redux/selectors';
 // import ContactForm from '../ContactForm/ContactForm';
